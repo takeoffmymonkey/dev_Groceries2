a2d2864a11396eb9c3a0999a7be65dce662e97f1
takeoff <galukhin@gmail.com>
2017-06-01 21:15:24 +0300

    2 fabs temp functionality 2 fabs temp icons

--- app/src/main/java/com/example/android/groceries2/MainActivity.java
+++ app/src/main/java/com/example/android/groceries2/MainActivity.java
@@ -5,8 +5,10 @@ package com.example.android.groceries2;
 
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import com.example.android.groceries2.data.GroceriesDbHelper;
@@ -14,29 +16,59 @@ import com.example.android.groceries2.data.GroceriesDbHelper;
 
 public class MainActivity extends AppCompatActivity {
 
+    private int itemsTotal = 0;
+    GroceriesDbHelper dbHelper;
+    SQLiteDatabase db;
+    TextView testText;
+    TextView testText2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         //Create dbHelper object
-        GroceriesDbHelper dbHelper = new GroceriesDbHelper(this, GroceriesDbHelper.DATABASE_NAME,
+        dbHelper = new GroceriesDbHelper(this, GroceriesDbHelper.DATABASE_NAME,
                 null, GroceriesDbHelper.DBVERSION);
 
         //Create db reference
-        SQLiteDatabase db = dbHelper.getWritableDatabase();
+        db = dbHelper.getWritableDatabase();
 
 
-        String s = ("INSERT INTO groceries (" +
-                "_id, name, price, weight, measure) VALUES (" +
-                "1," + "\"Test\"," + " 1, 1, 1);");
+        FloatingActionButton fabAdd = (FloatingActionButton) findViewById(R.id.fab_add_item);
+        FloatingActionButton fabDelete = (FloatingActionButton) findViewById(R.id.fab_delete_db);
+
+        fabAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String s = ("INSERT INTO groceries (" +
+                        "_id, name, price, weight, measure) VALUES (" +
+                        Integer.toString(itemsTotal + 1) + ", \"Test\"," + " 1, 1, 1);");
+
+                itemsTotal++;
+                db.execSQL(s);
+                updateItem();
+            }
+        });
+
+        fabDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                deleteDatabase("groceries_db");
+            }
+        });
 
 
-        db.execSQL(s);
+    }
 
+
+    public void updateItem() {
         Cursor cursor = db.query(GroceriesDbHelper.TABLE_GROCERIES, null, null, null, null, null, null);
 
-        TextView testText = (TextView) findViewById(R.id.test_text_field);
+        testText = (TextView) findViewById(R.id.test_text_field);
+        testText2 = (TextView) findViewById(R.id.test_text_field2);
 
         cursor.moveToFirst();
         int itemId = cursor.getInt(cursor.getColumnIndex("_id"));
@@ -48,12 +80,9 @@ public class MainActivity extends AppCompatActivity {
 
         testText.setText("ID:" + itemId + " Name:" + itemName + " Price:"
                 + itemPrice + " Weight: " + itemWeight + " Measure: " + itemMeasure);
+        testText2.setText("Rows:" + cursor.getCount());
 
 
         cursor.close();
-
-        deleteDatabase("groceries_db");
     }
-
-
 }
