301f79663704fc8fc56165ff1f65f38522266eed
takeoff <galukhin@gmail.com>
2017-06-01 14:31:42 +0300

    fix compilation bug in list_item.xml, add dynamic textView

--- app/src/main/java/com/example/android/groceries2/MainActivity.java
+++ app/src/main/java/com/example/android/groceries2/MainActivity.java
@@ -5,6 +5,9 @@ package com.example.android.groceries2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -13,8 +16,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+        TextView testText = (TextView) findViewById(R.id.test_text_field);
+        testText.setText("THIS IS A TEST");
+
+    }
 
 
 }
