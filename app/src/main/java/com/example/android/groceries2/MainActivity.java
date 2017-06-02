c6154b13c0ca1a420f4586c64c042ce0741896ce
takeoff <galukhin@gmail.com>
2017-06-02 20:01:33 +0300

    shrink main tab remove option's code from main activity

--- app/src/main/java/com/example/android/groceries2/MainActivity.java
+++ app/src/main/java/com/example/android/groceries2/MainActivity.java
@@ -23,7 +23,6 @@ public class MainActivity extends AppCompatActivity {
 
     private int itemsTotal = 0;
     static public GroceriesDbHelper dbHelper;
-    SQLiteDatabase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,37 +41,5 @@ public class MainActivity extends AppCompatActivity {
         tabLayout.setupWithViewPager(viewPager);
 
     }
-
-
-/*    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu options from the res/menu/menu_catalog.xml file.
-        // This adds menu items to the app bar.
-        getMenuInflater().inflate(R.menu.menu_list, menu);
-        return true;
-    }
-
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // User clicked on a menu option in the app bar overflow menu
-        switch (item.getItemId()) {
-            // Respond to a click on the "Insert dummy data" menu option
-            case R.id.settings_option_1:
-                String s = ("INSERT INTO groceries (" +
-                        "_id, name, price, weight, measure) VALUES (" +
-                        Integer.toString(itemsTotal + 1) + ", \"Test\"," + " 1, 1, 1);");
-
-                itemsTotal++;
-                db.execSQL(s);
-                return true;
-            // Respond to a click on the "Delete all entries" menu option
-            case R.id.settings_option_2:
-                deleteDatabase("groceries_db");
-                return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }*/
-
 
 }
