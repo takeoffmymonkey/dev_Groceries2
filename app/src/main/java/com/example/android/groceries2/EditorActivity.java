354f550e1ee9c8f40a9209b10a1da1ed8e994022
takeoff <galukhin@gmail.com>
2017-06-08 17:35:42 +0300

    add spinner to editor activity

--- app/src/main/java/com/example/android/groceries2/EditorActivity.java
+++ app/src/main/java/com/example/android/groceries2/EditorActivity.java
@@ -1,21 +1,67 @@
 package com.example.android.groceries2;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 /**
  * Created by takeoff on 006 06 Jun 17.
  */
 
 public class EditorActivity extends AppCompatActivity {
+
+    private String name;
+
+    private int price;
+
+    private int weight;
+
+    private String measurement;
+
+    private EditText nameEditText;
+
+    private EditText priceEditText;
+
+    private EditText weightEditText;
+
+    private Spinner measurementSpinner;
+
+    private boolean itemHasChanged = false;
+
+    private View.OnTouchListener touchListener = new View.OnTouchListener() {
+        @Override
+        public boolean onTouch(View view, MotionEvent motionEvent) {
+            itemHasChanged = true;
+            return false;
+        }
+    };
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editor);
+
+        nameEditText = (EditText) findViewById(R.id.editor_name);
+        priceEditText = (EditText) findViewById(R.id.editor_price);
+        weightEditText = (EditText) findViewById(R.id.editor_weight);
+        measurementSpinner = (Spinner) findViewById(R.id.editor_measurement);
+
+
+        // Setup OnTouchListeners on all the input fields, so we can determine if the user
+        // has touched or modified them. This will let us know if there are unsaved changes
+        // or not, if the user tries to leave the editor without saving.
+        nameEditText.setOnTouchListener(touchListener);
+        priceEditText.setOnTouchListener(touchListener);
+        weightEditText.setOnTouchListener(touchListener);
+        measurementSpinner.setOnTouchListener(touchListener);
 
         FloatingActionButton fabApproveItem = (FloatingActionButton)
                 findViewById(R.id.fab_approve_item);
@@ -23,8 +69,60 @@ public class EditorActivity extends AppCompatActivity {
         fabApproveItem.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                saveItem();
             }
         });
 
+        setupSpinner();
+
     }
+
+    private void setupSpinner() {
+        ArrayAdapter measurementSpinnerAdapter =
+                ArrayAdapter.createFromResource(this, R.array.array_measurement_options,
+                        android.R.layout.simple_spinner_item);
+
+
+        // Specify dropdown layout style - simple list view with 1 item per line
+        measurementSpinnerAdapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+
+        // Apply the adapter to the spinner
+        measurementSpinner.setAdapter(measurementSpinnerAdapter);
+
+
+        // Set the integer mSelected to the constant values
+        measurementSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String selection = (String) parent.getItemAtPosition(position);
+                if (!TextUtils.isEmpty(selection)) {
+                    if (selection.equals("item(s)")) {
+                        measurement = "item(s)";
+                    } else if (selection.equals("Kg")) {
+                        measurement = "Kg";
+                    } else if (selection.equals("gr")) {
+                        measurement = "gr";
+                    } else if (selection.equals("L")) {
+                        measurement = "L";
+                    } else if (selection.equals("ml")) {
+                        measurement = "ml";
+
+                    }
+                }
+            }
+
+            // Because AdapterView is an abstract class, onNothingSelected must be defined
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                measurement = "item(s)";
+            }
+        });
+    }
+
+
+    private void saveItem() {
+
+
+    }
+
 }
--- app/src/main/java/com/example/android/groceries2/data/GroceriesDbHelper.java
+++ app/src/main/java/com/example/android/groceries2/data/GroceriesDbHelper.java
@@ -44,7 +44,7 @@ public class GroceriesDbHelper extends SQLiteOpenHelper {
     public static final String GROCERIES_TABLE_CREATE = "CREATE TABLE " + TABLE_GROCERIES + " (" + "_id" +
             " INTEGER PRIMARY KEY AUTOINCREMENT, " +
             ITEM_NAME + " TEXT, " + ITEM_PRICE + " INTEGER, " + ITEM_WEIGHT + " INTEGER, " +
-            ITEM_MEASURE + " INTEGER, " + ITEM_CHECKED + " INTEGER);";
+            ITEM_MEASURE + " TEXT, " + ITEM_CHECKED + " INTEGER);";
 
     public static final String GROCERIES_TABLE_DROP = "DROP TABLE " + TABLE_GROCERIES + ";";
 
