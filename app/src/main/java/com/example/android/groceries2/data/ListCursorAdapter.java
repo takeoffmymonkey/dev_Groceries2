72ddb1a2687b4b4ad735a90d78e4825732b1d861
takeoff <galukhin@gmail.com>
2017-06-16 19:57:39 +0300

    Great Refactor in progress4(buggy)

--- app/src/main/java/com/example/android/groceries2/data/ListCursorAdapter.java
+++ app/src/main/java/com/example/android/groceries2/data/ListCursorAdapter.java
@@ -2,6 +2,7 @@ package com.example.android.groceries2.data;
 
 import android.content.Context;
 import android.database.Cursor;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,8 +12,14 @@ import android.widget.TextView;
 
 import com.example.android.groceries2.R;
 
+import static com.example.android.groceries2.MainActivity.db;
+import static com.example.android.groceries2.data.GroceriesDbHelper.ID_COLUMN;
+import static com.example.android.groceries2.data.GroceriesDbHelper.ITEMS_MEASURE_COLUMN;
+import static com.example.android.groceries2.data.GroceriesDbHelper.ITEMS_TABLE_NAME;
 import static com.example.android.groceries2.data.GroceriesDbHelper.LIST_AMOUNT_COLUMN;
 import static com.example.android.groceries2.data.GroceriesDbHelper.LIST_ITEM_COLUMN;
+import static com.example.android.groceries2.data.GroceriesDbHelper.MEASURE_MEASURE_COLUMN;
+import static com.example.android.groceries2.data.GroceriesDbHelper.MEASURE_TABLE_NAME;
 import static com.example.android.groceries2.data.GroceriesDbHelper.NAME_COLUMN;
 import static com.example.android.groceries2.data.GroceriesDbHelper.PRICE_COLUMN;
 
@@ -33,21 +40,56 @@ public class ListCursorAdapter extends CursorAdapter {
     @Override
     public void bindView(View view, Context context, Cursor cursor) {
 
+        //Get LIST_ITEM_COLUMN value of the item in List table
+        int itemCode = cursor.getInt(cursor.getColumnIndex(LIST_ITEM_COLUMN));
+
+        //Get cursor with NAME_COLUMN and ITEMS_MEASURE_COLUMN columns for required ID
+        Cursor itemsTableCursor = db.query(ITEMS_TABLE_NAME,
+                new String[]{NAME_COLUMN, ITEMS_MEASURE_COLUMN},
+                ID_COLUMN + "=?", new String[]{Integer.toString(itemCode)},
+                null, null, null);
+
+        //Save string with proper name
+        String name = itemsTableCursor.getString(itemsTableCursor.getColumnIndex(NAME_COLUMN));
+
+        //Get code of the items measure
+        int measureInItems = itemsTableCursor
+                .getInt(itemsTableCursor.getColumnIndex(ITEMS_MEASURE_COLUMN));
+
+        //Close itemsTableCursor cursor
+        itemsTableCursor.close();
+
+        //Get cursor with MEASURE_MEASURE_COLUMN text from Measure_table
+        Cursor measureTableCursor = db.query(MEASURE_TABLE_NAME,
+                new String[]{MEASURE_MEASURE_COLUMN},
+                ID_COLUMN + "=?", new String[]{Integer.toString(measureInItems)},
+                null, null, null);
+
+        //Save string with proper measure
+        String measure = measureTableCursor
+                .getString(measureTableCursor.getColumnIndex(MEASURE_MEASURE_COLUMN));
+
+        //Close measureTableCursor cursor
+        measureTableCursor.close();
+
+        //Create checkBox object
         CheckBox itemNameCheckBox = (CheckBox) view.findViewById(R.id.list_item_checkbox);
+        //Set its text
+        itemNameCheckBox.setText(name);
 
+        //Create itemAmount textView object
         TextView itemAmountTextView = (TextView) view.findViewById(R.id.list_item_amount);
+        //Get itemAmount value from cursor
+        int itemAmount = cursor.getInt(cursor.getColumnIndex(LIST_AMOUNT_COLUMN));
+        //Set itemAmount + measure as text to itemAmount textView
+        itemAmountTextView.setText(Integer.toString(itemAmount) + " " + measure);
 
+        //Create itemPrice textView object
         TextView itemPriceTextView = (TextView) view.findViewById(R.id.list_item_price);
-
-        itemNameCheckBox.setText(cursor.getString(cursor.getColumnIndex(LIST_ITEM_COLUMN)));
-
-        int amount = cursor.getInt(cursor.getColumnIndex(LIST_AMOUNT_COLUMN));
-
-        itemAmountTextView.setText(Integer.toString(amount));
-
-        int price = cursor.getInt(cursor.getColumnIndex(PRICE_COLUMN));
-
-        itemPriceTextView.setText(Integer.toString(price));
+        //Get itemPrice value from cursor
+        int itemPrice = cursor.getInt(cursor.getColumnIndex(PRICE_COLUMN));
+        //Set total itemPrice as product of itemPrice and ItemAmount to itemPrice textView
+        itemPriceTextView.setText(Integer.toString(itemPrice * itemAmount));
 
     }
 }
